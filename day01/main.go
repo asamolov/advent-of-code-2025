27dd967470c90c278f94c25727c435e74df6c0f9
Alexander Samolov <alexsamolov@gmail.com>
2025-12-02 00:17:41 +0100

    day01: part2. damn corner cases

--- day01/main.go
+++ day01/main.go
@@ -14,6 +14,7 @@ func main() {
 	fmt.Println("reading input")
 	pos := 50
 	result := 0
+	result2 := 0
 	for i, l := range lines {
 		fmt.Printf("cmd %3d: %s\n", i, l)
 		dir := l[0]
@@ -24,15 +25,24 @@ func main() {
 		if dir == 'L' {
 			n = -n
 		}
+		oldPos := pos
 		pos += n
+		result2 += utils.Abs(pos / 100)
 		pos %= 100
 		if pos < 0 {
 			pos += 100
+			if oldPos != 0 {
+				result2++
+			}
 		}
-		fmt.Printf("dir: %3d, pos: %3d\n", n, pos)
 		if pos == 0 {
 			result += 1
+			if n < 0 {
+				result2 += 1
+			}
 		}
+		fmt.Printf("pos: %3d, dir: %3d, res1: %d, res2: %d\n", pos, n, result, result2)
 	}
 	fmt.Printf("result: %d\n", result)
+	fmt.Printf("result2: %d\n", result2)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/asamolov/advent-of-code-2025
 
 go 1.25.4
+
+require golang.org/x/exp v0.0.0-20251125195548-87e1e737ad39 // indirect
