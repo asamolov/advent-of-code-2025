2e8d8c89fc36a3b7fa92817118c4f6ab6d31bc64
Alexander Samolov <alexsamolov@gmail.com>
2025-12-01 23:32:12 +0100

    day01: part1

--- day01/main.go
+++ day01/main.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/asamolov/advent-of-code-2025/internal/utils"
 )
 
 func main() {
 	defer utils.Timer("task")()
+
 	lines := utils.ReadInput()
 	fmt.Println("reading input")
+	pos := 50
+	result := 0
 	for i, l := range lines {
-		fmt.Printf("%3d: %s\n", i, l)
+		fmt.Printf("cmd %3d: %s\n", i, l)
+		dir := l[0]
+		n, err := strconv.Atoi(l[1:])
+		if err != nil {
+			panic(fmt.Sprintf("cannot parse line %d: %s", i, l))
+		}
+		if dir == 'L' {
+			n = -n
+		}
+		pos += n
+		pos %= 100
+		if pos < 0 {
+			pos += 100
+		}
+		fmt.Printf("dir: %3d, pos: %3d\n", n, pos)
+		if pos == 0 {
+			result += 1
+		}
 	}
+	fmt.Printf("result: %d\n", result)
 }
