ba65a486f27d1e383dd719725bb0787b7e9ef8be
Alexander Samolov <alexsamolov@gmail.com>
2025-12-12 22:40:27 +0100

    day03: part 2

--- day03/main.go
+++ day03/main.go
@@ -35,6 +35,31 @@ func findMaxJolt(m map[int][]int) int {
 	return 0 // impossible case
 }
 
+func goDeep(m map[int][]int, maxIdx, curr, startIdx, rem int) (result int, found bool) {
+	rem--
+	if rem < 0 {
+		return curr, true
+	}
+	for j := 9; j >= 1; j-- {
+		js := m[j]
+		idx := slices.IndexFunc(js, func(jidx int) bool {
+			return jidx >= startIdx && jidx < maxIdx-rem
+		})
+		if idx >= 0 {
+			result, found := goDeep(m, maxIdx, curr*10+j, js[idx]+1, rem)
+			if found {
+				return result, found
+			}
+		}
+	}
+	return 0, false
+}
+
+func findMaxJolt12(m map[int][]int, maxIdx int) int {
+	result, _ := goDeep(m, maxIdx, 0, 0, 12)
+	return result
+}
+
 func main() {
 	defer utils.Timer("task")()
 
@@ -46,8 +71,13 @@ func main() {
 	for _, l := range lines {
 		m := toMap(l)
 		maxJolt := findMaxJolt(m)
+		maxJolt2, _ := goDeep(m, len(l), 0, 0, 2)
+		maxJolt12 := findMaxJolt12(m, len(l))
 		fmt.Printf("%s: %d\n", l, maxJolt)
+		fmt.Printf("%s: %d\n", l, maxJolt2)
+		fmt.Printf("%s: %d\n", l, maxJolt12)
 		result += maxJolt
+		result2 += maxJolt12
 	}
 	fmt.Printf("result: %d\n", result)
 	fmt.Printf("result2: %d\n", result2)
