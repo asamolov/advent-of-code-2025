99a3a63162fecd669765d48626ed54bd3124e976
Alexander Samolov <alexsamolov@gmail.com>
2025-12-13 15:57:03 +0100

    day05: part 1

---
+++ day05/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/asamolov/advent-of-code-2025/internal/utils"
+)
+
+func parseInterval(interval string) (*Interval, error) {
+	x := strings.Split(interval, "-")
+	if len(x) != 2 {
+		return nil, fmt.Errorf("can't parse interval from '%s'", interval)
+	}
+	start, err := strconv.Atoi(x[0])
+	if err != nil {
+		return nil, fmt.Errorf("can't parse interval from '%s': [%w]", interval, err)
+	}
+	finish, err := strconv.Atoi(x[1])
+	if err != nil {
+		return nil, fmt.Errorf("can't parse interval from '%s': [%w]", interval, err)
+	}
+	return &Interval{start: start, finish: finish}, nil
+}
+
+type Interval struct {
+	start, finish int
+}
+
+func (interval *Interval) Contains(i int) bool {
+	return i >= interval.start && i <= interval.finish
+}
+
+func checkRanges(ranges []*Interval, i int) bool {
+	for _, r := range ranges {
+		if r.Contains(i) {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	defer utils.Timer("task")()
+
+	lines := utils.ReadInput()
+	fmt.Println("reading input")
+
+	result := 0
+	result2 := 0
+	idx := 0
+
+	var ranges []*Interval
+	for idx = 0; idx < len(lines); idx++ {
+		line := lines[idx]
+		if len(line) == 0 {
+			break
+		}
+		interval, err := parseInterval(line)
+		if err != nil {
+			panic(err)
+		}
+		ranges = append(ranges, interval)
+	}
+
+	for idx++; idx < len(lines); idx++ {
+		line := lines[idx]
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		if checkRanges(ranges, i) {
+			result++
+		}
+	}
+	fmt.Printf("result: %d\n", result)
+	fmt.Printf("result2: %d\n", result2)
+}
